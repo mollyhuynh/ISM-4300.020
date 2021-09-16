303c320e2f0578431de1caf92cae6891850a1ac3
Molly Huynh <89495252+mollyhuynh@users.noreply.github.com>
2021-09-16 16:20:24 -0700

    Add files via upload

---
+++ TimeLoop_MH.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TimeloopProblem  
+{
+    public class timeloop
+    {
+        public static void Main(String[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int x = 1; x <= num; x++)
+                Console.WriteLine(x + " Abracadabra");
+        }
+    }
+}
